0ffb446cb3ea4ae3efa664d31a56e0a280013de1
Krzysztof Perzanowski <krzysztof.perzanowski@landingi.com>
2020-12-27 15:34:58 +0100

    Initial files

---
+++ frontend/app/web/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http
+    .createServer((req, res) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Hello World!');
+    })
+    .listen(5000);
