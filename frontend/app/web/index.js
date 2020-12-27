620edcb03ed0d28928eb44ce05e7684d608e9566
Krzysztof Perzanowski <krzysztof.perzanowski@landingi.com>
2020-12-27 22:42:33 +0100

    Code improvements

--- frontend/app/web/index.js
+++ frontend/app/web/index.js
@@ -5,6 +5,6 @@ http
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.end('Hello World!');
+        res.end('Hello World 2!');
     })
     .listen(5000);
